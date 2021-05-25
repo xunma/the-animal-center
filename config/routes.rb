a6caff1f549340bf01b36c251a6faab14cc27291
Xun Ma <lazybone1995@hotmail.com>
2021-05-25 19:22:15 +0800

    add `index` action to animals

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 1. users can see all the animals
+  get 'animals', to: 'animals#index'
+  # 2. users can see one animal
+  # 3. users can create a new animal
+  # 4. users can update an animal
+  # 5. users can delete an animal
 end
