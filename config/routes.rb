5f65f79fb790d99f1739eca6cae04ca31437ceb6
Xun Ma <lazybone1995@hotmail.com>
2021-05-31 20:38:44 +0800

    use resources for every route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  patch 'animals/:id/mark_as_adopted', to: 'animals#mark_as_adopted', as: :mark_as_adopted
   resources :animals do
+    member do
+      patch '/mark_as_adopted', to: 'animals#mark_as_adopted', as: :mark_as_adopted
+    end
     resources :donations, only: [:new, :create, :index]
   end
   resources :donations, only: [:destroy, :edit, :update]
