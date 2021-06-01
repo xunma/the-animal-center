6f625310d2ed18e872e903598050139062ff43ae
Sergey Filippov <sergey.filippov.89@inbox.ru>
2021-06-01 19:59:24 +0800

    styled index page

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,8 +1,8 @@
 class Animal < ApplicationRecord
   has_many :donations
   validates :name, uniqueness: { scope: :species,
-    message: "name and speices combination was already taken" }
-  def available_species
+                                 message: "name and speices combination was already taken" }
+  def self.available_species
     # an array of species
     ["cat", "dog", "butterfly", "lizard", "fish", "goat"]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Animal.destroy_all
+
+10.times do
+  a = Animal.create!(
+    name: Faker::Creature::Dog.unique.name,
+    sex: %w[male female].sample,
+    species: Animal.available_species.sample
+  )
+  puts "Created #{a.name}"
+
+end
+puts "Created #{Animal.count} animals"
